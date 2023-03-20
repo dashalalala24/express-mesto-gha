68a947f616f041c3749f76a2d570b550fa53324d
Daria Lazarchuk <lazarchukdarya@gmail.com>
2023-03-20 17:54:58 +0300

    fix: joi validation regex

--- routes/cards.js
+++ routes/cards.js
@@ -16,7 +16,7 @@ router.get('/cards', getCards);
 router.post('/cards', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30).required(),
-    link: Joi.string().required().pattern(new RegExp(picRegEx)),
+    link: Joi.string().required().regex(picRegEx),
   }),
 }), createCard);
 
--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,7 @@ router.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().pattern(new RegExp(picRegEx)),
+    avatar: Joi.string().regex(picRegEx),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),

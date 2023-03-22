4cedd271df2c148d2d77d571d50a113076effd9d
Daria Lazarchuk <lazarchukdarya@gmail.com>
2023-03-22 13:31:32 +0300

    fix: change db adress

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const { PORT = '3000' } = process.env;
-const { DB_ADDRESS = 'mongodb://localhost:27017/mestodb' } = process.env;
+const { DB_ADDRESS = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 module.exports = {
   PORT,

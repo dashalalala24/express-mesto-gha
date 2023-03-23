9f8b084bc53872de0cd468910f78b6d88ec08577
Daria Lazarchuk <lazarchukdarya@gmail.com>
2023-03-23 18:29:27 +0300

    undo  no * routes

--- routes/index.js
+++ routes/index.js
@@ -19,7 +19,7 @@ router.use(auth);
 router.use('/users', userRouter);
 router.use('/cards', cardRouter);
 
-router.use((req, res, next) => {
+router.use('*', (req, res, next) => {
   next(new NotFoundError('Страница не найдена'));
 });
 

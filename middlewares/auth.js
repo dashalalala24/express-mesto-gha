4f87cac13376b71e5ab9a6722fd512daa69e6c94
Daria Lazarchuk <lazarchukdarya@gmail.com>
2023-03-24 11:26:10 +0300

    test auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,10 +4,17 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 const UnauthorizedError = require('../errors/UnauthorizedError');
 
 module.exports.auth = (req, res, next) => {
+  console.log(req.headers);
+
   if (!req.headers.cookie) {
-    throw new UnauthorizedError('Необходима авторизация');
+    throw new UnauthorizedError('Необходима авторизация, нет кук в хедерсе');
+  }
+
+  if (!req.headers.authorization) {
+    throw new UnauthorizedError('Необходима авторизация, нет authorization в хедерсе');
   }
   const { cookie } = req.headers;
+  // const { authorization } = req.headers;
   const token = cookie.replace('jwt=', '');
   let payload;
   try {

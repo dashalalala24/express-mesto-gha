b2fe26c631f9445a2e7fd82f746c33a9dbbea4ee
Daria Lazarchuk <lazarchukdarya@gmail.com>
2023-03-24 08:56:07 +0300

    test login res.send token

--- controllers/users.js
+++ controllers/users.js
@@ -118,7 +118,7 @@ const login = (req, res, next) => {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
       });
-      res.send({ message: 'Вы авторизированы' }, token);
+      res.send({ message: 'Вы авторизированы', token });
     })
     .catch(next);
 };

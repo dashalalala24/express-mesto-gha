27aadfd5435f26fcfb095d2c68cbf83f2faeb382
Daria Lazarchuk <lazarchukdarya@gmail.com>
2023-03-28 15:47:50 +0300

    change auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,20 +1,40 @@
-const jwt = require('jsonwebtoken');
+const token = require('jsonwebtoken');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 const UnauthorizedError = require('../errors/UnauthorizedError');
 
 module.exports.auth = (req, res, next) => {
-  console.log('req.headers из auth:', req.headers);
+  console.log('req.headers из auth:', req.cookies);
 
-  if (!req.headers.cookie) {
-    throw new UnauthorizedError('Необходима авторизация, нет кук в хедерс');
+  // if (!req.headers.cookie) {
+  //   throw new UnauthorizedError('Необходима авторизация, нет кук в хедерс');
+  // }
+
+  // const { cookie } = req.headers;
+  // const token = cookie.replace('jwt=', '');
+  // let payload;
+  // try {
+  //   payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+  //   console.log('payload из auth:', payload);
+  // } catch {
+  //   next(new UnauthorizedError('Необходима авторизация!!!'));
+  // }
+  // req.user = payload;
+
+  // next();
+
+  if (!req.cookies) {
+    throw new UnauthorizedError('Необходима авторизация, нет кук в рек');
   }
 
-  const { cookie } = req.headers;
-  const token = cookie.replace('jwt=', '');
+  const { jwt } = req.cookies;
+  if (!token) {
+    return next(new UnauthorizedError('Нужно авторизоваться'));
+  }
+  // const token = jwt.replace('jwt=', '');
   let payload;
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+    payload = token.verify(jwt, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
     console.log('payload из auth:', payload);
   } catch {
     next(new UnauthorizedError('Необходима авторизация!!!'));

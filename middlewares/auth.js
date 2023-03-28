8963b988e45b12f97406e2f35f0d01df50f1bca0
Daria Lazarchuk <lazarchukdarya@gmail.com>
2023-03-28 18:48:07 +0300

    fix auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -49,7 +49,7 @@ const jwt = require('jsonwebtoken');
 const { NODE_ENV, JWT_SECRET } = process.env;
 const UnauthorizedError = require('../errors/UnauthorizedError');
 
-module.exports = (req, res, next) => {
+module.exports.auth = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
